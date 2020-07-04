f243684bdfce93673033122d756d362732d6c970
Spookdot <333.333.333@gmx.de>
2020-07-05 00:01:32 +0200

    Optimization of the !mew beautiful command to clean up the code and improve universal use of methods and converters that it uses

--- cogs/ImagineManim.py
+++ cogs/ImagineManim.py
@@ -1,10 +1,9 @@
 import yaml
 import discord
-import requests
 from PIL import Image
 from io import BytesIO
-from typing import Union
 from discord.ext import commands
+from cogs.converters import ImageUserConv
 
 
 class ImagineManim(commands.Cog, name="ImagineManim"):
@@ -13,14 +12,12 @@ class ImagineManim(commands.Cog, name="ImagineManim"):
         temp_yaml = yaml.load(open("config.yaml", "r"))
         self.meme_pos = temp_yaml["Memes"]
 
-    def make_meme(self, meme: str, user1: Image, user2: Image = None, user3: Image = None):
+    def make_meme(self, meme: str, user1: Image, user2: Image = None):
         mem = self.meme_pos[meme.lower()]
         img = Image.open(mem["image"]).copy()
         img.paste(user1.resize(mem["user1_resize"]), mem["user1"])
         if user2 is not None:
             img.paste(user2.resize(mem["user2_resize"]), mem["user2"])
-        if user3 is not None:
-            img.paste(user3.resize(mem["user3_resize"]), mem["user3"])
         file = BytesIO()
         img.save(file, format="PNG")
         file.seek(0)
@@ -33,25 +30,18 @@ class ImagineManim(commands.Cog, name="ImagineManim"):
         await ctx.send(file=self.make_meme("approach", user1, user2))
 
     @commands.command(description="Admiwe the bweauty of this pic or purrson")
-    async def beautiful(self, ctx, rp_person: Union[discord.User, str] = None):
+    async def beautiful(self, ctx, rp_person: ImageUserConv = Image.NONE):
+        if rp_person == Image.NONE:
+            await ctx.send("OnO, seems chu hav nothing to admiwe")
+            return
         user1 = Image.open(BytesIO(await ctx.author.avatar_url_as(format="png").read()))
         mem = self.meme_pos["beautiful"]
         img = Image.open(mem["image"]).copy()
         img.paste(user1.resize(mem["user1_resize"]), mem["user1"])
         img.paste(user1.resize(mem["user2_resize"]), mem["user2"])
-        if isinstance(rp_person, discord.User):
-            user2 = Image.open(BytesIO(await rp_person.avatar_url_as(format="png").read()))
-            user2 = user2.resize(mem["user3_resize"])
-        elif isinstance(rp_person, str):
-            user2 = Image.open(BytesIO(requests.get(rp_person).content))
-        elif ctx.message.attachments:
-            user2 = Image.open(BytesIO(await ctx.message.attachments[0].read()))
-        else:
-            await ctx.send("OnO, seems chu hav nowing to admiwe")
-            return
-        user2.thumbnail((800, 800))
-        img.paste(user2, ((img.width-user2.width)//2, img.height-(450+user2.height)))
-        img = img.crop((0, img.height-(450+user2.height), img.width, img.height))
+        rp_person.thumbnail((800, 800))
+        img.paste(rp_person, ((img.width-rp_person.width)//2, img.height-(450+rp_person.height)))
+        img = img.crop((0, img.height-(450+rp_person.height), img.width, img.height))
         file = BytesIO()
         img.save(file, format="PNG")
         file.seek(0)
--- cogs/converters.py
+++ cogs/converters.py
@@ -1,3 +1,6 @@
+import requests
+from PIL import Image
+from io import BytesIO
 from discord.ext import commands
 
 
@@ -8,3 +11,18 @@ class TagNameConv(commands.Converter):
             return usr.name
         else:
             return argument
+
+
+class ImageUserConv(commands.Converter):
+    async def convert(self, ctx, argument) -> Image.Image:
+        if argument[:3] + argument[-1] == "<@!>":
+            print("myes")
+            usr = await ctx.bot.fetch_user(argument[3:-1])
+            im = Image.open(BytesIO(await usr.avatar_url_as(format="png").read()))
+            return im.resize((512, 512))
+        elif ctx.message.attachments:
+            return Image.open(BytesIO(await ctx.message.attachments[0].read()))
+        elif argument[:3] + argument[-1] != "<@!>":
+            return Image.open(BytesIO(requests.get(argument).content))
+        else:
+            return Image.NONE
